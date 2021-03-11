002f03132336956de2994de0825911c1111352ef
AJLStick <aailoc6693@student.ccs.k12.nc.us>
2021-03-11 14:52:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var gameData = {
 
 function getshards() {
   gameData.shards += gameData.shardsclick
-  document.getElementById("shardsmined").innerHTML = gameData.shards + " shards collected"
+  document.getElementById("shardsmined").innerHTML = "you've got " + gameData.shards + " shards"
 }
 
 function buyshardsclick() {
@@ -14,8 +14,17 @@ function buyshardsclick() {
     gameData.shards -= gameData.shardsclickcost
     gameData.shardsclick += 1
     gameData.shardsclick *= 2
-    document.getElementById("shards").innerHTML = gameData.shards + " shards collected"
-    document.getElementById("pickaxe").innerHTML = "upgrade pickaxe (Currently Level " + gameData.shardsclick + ") costs " + costsshardsclickcost + " shards"
+    document.getElementById("shardsmined").innerHTML = gameData.shards + " shards collected"
+    document.getElementById("pickaxe").innerHTML = "upgrade pickaxe (" + gameData.pickaxename + ")[" + gamedata.shardsclick + " "] costs " + costsshardsclickcost + " shards"
+  }
+  if (gamedata.shardsclick >=10 && gamedata.shardsclick <=50) {
+    var pickaxename = "wooden";
+  }
+  if (gamedata.shardsclick >=49 && gamedata.shardsclick <=100) {
+    var pickaxename = "stone";
+  }
+  if (gamedata.shardsclick >=99 && gamedata.shardsclick <=200) {
+    var pickaxename = ";
   }
 }
 
